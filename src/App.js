1c163fdf73a74dd0b3e182de9b880e466b5f1495
SeniorCod-72 <wwekesa72@gmail.com>
2025-02-04 10:05:49 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-function App() {
-  return (
-    <div></div>
-  )
-}
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App'; // Make sure the relative path is correct
 
-export default App;
+test('renders the app component', () => {
+  const { getByText } = render(<App />);
+  expect(getByText(/Hello, World!/i)).toBeInTheDocument(); // Adjust text as needed
+});
